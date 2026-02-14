70f3aeabeb64e88e0dd67fcd372409c78c4a258e
José Uriel Villanueva Donjuan <j.uriel.vilanueva.d@gmail.com>
2026-02-13 19:53:47 -0600

    Práctica documentada por capas

--- datos.js
+++ datos.js
@@ -1,3 +1,11 @@
+/*
+Este archivo pertenece a la capa de datos.
+Unicamente se encarga de recibir, gestionar y proporcionar los datos
+a la capa de la lógica de negocio
+*/
+
 function obtenerResultado(a,b){
+
+    // Se retorna la suma de los dos valores recibidos como parámetros
     return a+b;
 }
--- lógica.js
+++ lógica.js
@@ -1,8 +1,26 @@
+/* 
+Archivo que pertenece y se encarga de la capa de la lógica de negocio.
+Procesa la información ingresada por el usuario y conecta a la capa de presentación con la capa de datos
+*/
+
+/*
+ Función principal que se ejecuta cuando el usuario 
+ presiona el botón "SUMAR" en la interfaz (index.html).
+ */
 function sumar(){
+    /* 
+    Se obtienen los valores de los campos de entrada utilizando el id de los campos
+    y los convierte a núms. enteros. 
+    */
     let a = parseInt(document.getElementById("num1").value);
     let b = parseInt(document.getElementById("num2").value);
 
-    let resultado = obtenerResultado(a,b );
+    /* Se llama a la función encontrada en el archivo datos.js*/
+    let resultado = obtenerResultado(a,b);
 
+    /*
+    Actualiza dinámicamente el contenido del elemento
+    <p id="resultado"> en la interfaz gráfica.
+    */
     document.getElementById("resultado").innerText = "Resultado: " + resultado;
 }
